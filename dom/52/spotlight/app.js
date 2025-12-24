66fe2f91ba9d47ce7b3acf9c0ab36dce1cfbd21a
Akram khan <khanakram40507@gmail.com>
2025-12-24 20:29:04 +0530

    basic use in spot effect

---
+++ dom/52/spotlight/app.js
@@ -0,0 +1,4 @@
+addEventListener("mousemove", (e) => {
+    document.documentElement.style.setProperty('--x', e.clientX + 'px');
+    document.documentElement.style.setProperty('--y', e.clientY + 'px');
+});
