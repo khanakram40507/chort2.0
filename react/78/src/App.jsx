ebea261ed04c380dea90a98860b921c91dc81ee6
Akram khan <khanakram40507@gmail.com>
2026-03-02 21:13:21 +0530

    basic anther  project of use effect

--- react/78/src/App.jsx
+++ react/78/src/App.jsx
@@ -1,10 +1,18 @@
+// basically when we are try to change our ui then if we want some thing running in side stack then we can use use effect
 
-import React, { useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState('');  
+ useEffect(() => {
+  console.log('use effect called');
+ }) // when we want to run use effect when count changes then we can pass count in dependency array
+
+
   return (
     <div>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
       <h1>Count: {count}</h1>
       <button onClick={() => setCount(count + 1 )} className='bg-black text-white rounded px-4 py-2 w-fit'> Increment</button>
     </div>
